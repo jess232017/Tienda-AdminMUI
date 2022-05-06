9a40cfa8510814db426636640a20181dd00dd9da
Jesus Hernandez <jess232016@gmail.com>
2022-05-06 09:13:52 -0600

    fixes

--- src/main.jsx
+++ src/main.jsx
@@ -9,7 +9,6 @@ import { QueryClient, QueryClientProvider } from 'react-query';
 //Theme
 import 'react-toastify/dist/ReactToastify.css';
 import 'react-perfect-scrollbar/dist/css/styles.css';
-import '@/fonts/dyslexic/OpenDyslexic-Bold.ttf';
 import '@/assets/scss/style.scss';
 
 const queryClient = new QueryClient();
